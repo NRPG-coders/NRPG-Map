a3e09552e6ef210c689ab76e38e4e7b47f89e0bb
Zee1234 <zeeatgit@gmail.com>
2017-10-13 20:55:16 -0500

    Swap to XMLHttpRequest

--- src/javascript/loadsvg.js
+++ src/javascript/loadsvg.js
@@ -1,9 +1,13 @@
 let mapholder = document.getElementById('map')
+let oReq = new XMLHttpRequest()
 
-fetch('something.github.io/NRPG-Map/dist/gfx/map.svg')
-  .then( response => response.text() )
-  .then( text => { mapholder.innerHTML = text } )
-  .catch( err => {
+oReq.addEventListener('load', res => {
+  try {
+    mapholder.innerHTML = res.target.responseText
+  } catch(err) {
     mapholder.innerHTML = "An error has been encountered, or you are experiencing connectivity issues."
     throw err;
-  })
+  }
+})
+oReq.open('GET', 'something.github.io/NRPG-Map/dist/gfx/map.svg')
+oReq.send()
